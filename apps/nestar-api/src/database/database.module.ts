7714eeef7d7f87163e89742ce2ef9edca84481be
andyw440 <nozimovasror47@gmail.com>
2025-12-27 16:43:03 +0900

    feat: create components related modules

--- apps/nestar-api/src/database/database.module.ts
+++ apps/nestar-api/src/database/database.module.ts
@@ -5,7 +5,8 @@ import { Connection } from 'mongoose';
     imports:[
         MongooseModule.forRootAsync({
             useFactory:() => ({
-                uri:process.env.NODE_ENV === 'production' ? process.env.MONGO_PROD : process.env.MONGO_DEV,
+                uri:process.env.NODE_ENV === 'production' ? 
+                process.env.MONGO_PROD : process.env.MONGO_DEV,
 
             })
         })
@@ -15,7 +16,8 @@ import { Connection } from 'mongoose';
 export class DatabaseModule {
     constructor(@InjectConnection() private readonly connection:Connection){
         if(connection.readyState === 1){
-            console.log(`MongoDB is connected into ${process.env.NODE_ENV === 'production' ? 'production' : 'development'} db`);
+            console.log(`MongoDB is connected into ${process.env.NODE_ENV === 'production' ? 
+                'production' : 'development'} db`);
          } else{
             console.log("DB is not connected");
             
