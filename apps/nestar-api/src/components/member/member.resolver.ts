368f890832a1087454bf07d96d8069c768e84d89
andyw440 <nozimovasror47@gmail.com>
2025-12-28 19:58:24 +0900

    feat: develop signup business logic

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,27 +1,41 @@
 import { Mutation, Resolver, Query, Args } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { UsePipes, ValidationPipe } from '@nestjs/common';
+import { InternalServerErrorException, UsePipes, ValidationPipe } from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { Member } from '../../libs/dto/member/member';
 
 
 @Resolver()
 export class MemberResolver {
     constructor(private readonly memberService: MemberService){}
     
-    @Mutation(() => String)
+    @Mutation(() => Member)
     @UsePipes(ValidationPipe)
-    public async signup(@Args("input") input:MemberInput):Promise<String>{
+    public async signup(@Args("input") input:MemberInput):Promise<Member>{
+      try{
         console.log("Mutation: signup");
         console.log("input:", input);
+        return this.memberService.signup(input) 
+      } catch(err){
+        console.log("Error, signup:", err);
+        throw new InternalServerErrorException(err)
         
-        return this.memberService.signup() 
+      } 
     }
 
       @Mutation(() => String)
       @UsePipes(ValidationPipe)
     public async login(@Args("input") input: LoginInput):Promise<String>{
+        try{
         console.log("Mutation: login");
-        return this.memberService.login()  
+        console.log("input:", input);
+        
+        return this.memberService.login() 
+      } catch(err){
+        console.log("Error, login:", err);
+        throw new InternalServerErrorException(err)
+        
+      } 
     }
 
       @Mutation(() => String)
