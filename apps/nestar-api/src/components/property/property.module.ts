dc6bc496a84152f645834f2020d96c4fe06ff672
andyw440 <nozimovasror47@gmail.com>
2026-01-06 18:46:08 +0900

    fix: modification of property module

--- apps/nestar-api/src/components/property/property.module.ts
+++ apps/nestar-api/src/components/property/property.module.ts
@@ -3,9 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import PropertySchema from '../../schemas/Property.model';
 import { PropertyResolver } from './property.resolver';
 import { PropertyService } from './property.service';
+import { AuthModule } from '../auth/auth.module';
+import { ViewModule } from '../view/view.module';
 
 @Module({
-    imports:[MongooseModule.forFeature([{ name: "Property", schema:PropertySchema}])],
+    imports:[
+        MongooseModule.forFeature([{ name: "Property", schema:PropertySchema}]),
+        AuthModule, ViewModule
+],
     providers:[PropertyResolver, PropertyService]
 })
 export class PropertyModule {}
--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -1,13 +1,13 @@
 import { Mutation, Resolver, Query } from '@nestjs/graphql';
 import { PropertyService } from './property.service';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/guards/auth.guard';
 
 
 @Resolver()
 export class PropertyResolver {
     constructor(private readonly propertyService:PropertyService){}
 
-    @Query(() => String)
-    public async sayPropertyName(){
-        return this.propertyService.sayPropertyName()
-    }
+    
+
 }
