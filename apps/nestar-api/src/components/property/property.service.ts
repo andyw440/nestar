b6cbd58dd6090b5f2d34bb8fa4e451e8b9ad4062
andyw440 <nozimovasror47@gmail.com>
2026-01-08 18:22:23 +0900

    feat: develop updateProperty business logic

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -7,10 +7,10 @@ import { Message } from '../../libs/enums/common.enum';
 import { MemberService } from '../member/member.service';
 import { StatisticModifier, T } from '../../libs/types/common';
 import { PropertyStatus } from '../../libs/enums/property.enum';
-import e from 'express';
 import { ViewGroup } from '../../libs/enums/view.enum';
 import { ViewService } from '../view/view.service';
-import { exec } from 'child_process';
+import { PropertyUpdate } from '../../libs/dto/property/property.update';
+import moment from 'moment';
 
 @Injectable()
 export class PropertyService {
@@ -61,5 +61,29 @@ export class PropertyService {
             {$inc: {[targetKey]: modifier }},
             { new: true }
         ).exec()
+    }
+
+    public async updateProperty(memberId: ObjectId, input: PropertyUpdate): Promise<Property> {
+        let {propertyStatus, soldAt, deletedAt } = input
+        const search: T = {
+            _id: input._id,
+            memberId:memberId,
+            propertyStatus:PropertyStatus.ACTIVE,
+        };
+
+        if(propertyStatus === PropertyStatus.SOLD) soldAt = moment().toDate();
+        else if(propertyStatus === PropertyStatus.DELETE) deletedAt = moment().toDate();
+
+        const result = await this.propertyModel.findOneAndUpdate(search, input, { new: true }).exec()
+        if(!result) throw new InternalServerErrorException(Message.UPDATE_FAILED)
+        
+            if(soldAt || deletedAt) {
+                await this.memberService.memberStatsEditor({
+                    _id:memberId,
+                    targetKey:'memberProperties',
+                    modifier:-1,
+                });
+            }
+            return result;
     }
 } 
