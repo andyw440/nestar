c090b87e0a5a790a1f40a272b0f8fc5ad912a563
andyw440 <nozimovasror47@gmail.com>
2026-01-09 16:47:13 +0900

    feat: develop getAllPropertiesByAdmin graphQL api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -3,7 +3,7 @@ import { PropertyService } from './property.service';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { Properties, Property } from '../../libs/dto/property/property';
-import { AgentPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
+import { AgentPropertiesInquiry, AllPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
@@ -61,14 +61,27 @@ export class PropertyResolver {
     }
 
     @Roles(MemberType.AGENT)
-@UseGuards(RolesGuard)
-@Query((returns) => Properties)
-public async getAgentProperties(
-  @Args('input') input: AgentPropertiesInquiry,
-  @AuthMember('_id') memberId: ObjectId,
-): Promise<Properties> {
-  console.log('Query: getAgentProperties');
-  return await this.propertyService.getAgentProperties(memberId, input);
-}
+    @UseGuards(RolesGuard)
+    @Query((returns) => Properties)
+    public async getAgentProperties(
+    @Args('input') input: AgentPropertiesInquiry,
+    @AuthMember('_id') memberId: ObjectId,
+    ): Promise<Properties> {
+    console.log('Query: getAgentProperties');
+    return await this.propertyService.getAgentProperties(memberId, input);
+    }
+    /** ADMIN */
+
+    @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
+    @Query((returns)=> Properties)
+    public async getAllPropertiesByAdmin(
+        @Args('input') input: AllPropertiesInquiry,
+        @AuthMember('_id') memberId:ObjectId,
+    ): Promise<Properties> {
+        console.log('Query: getAllPropertiesByAdmin');
+        return await this.propertyService.getAllPropertiesByAdmin(input)
+        
+    }
 
 }
